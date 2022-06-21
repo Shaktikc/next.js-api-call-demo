80ffb47419680baa0e3b7e1596ca851e4be195bb
Shakti Kc <shakti3462@gmail.com>
2022-06-21 14:56:21 +0545

    post linking to comment

---
+++ pages/postLinkingToComment/posts.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Box, Center, Flex, HStack, Text, VStack } from "@chakra-ui/react";
+
+const Posts = ({ posts }) => {
+  return (
+    <Box>
+      {posts.slice(0, 4).map((post) => {
+        return (
+          <Box
+            key={post.id}
+            w="30%"
+            mt="2rem"
+            ml="2rem"
+            mb="2rem"
+            boxShadow={"lg"}
+            bg="green.500"
+            padding={"1rem"}
+          >
+            <Box>
+              <Text fontWeight={"bold"}>ID</Text>
+              <Text>{post.id}</Text>
+            </Box>
+            <Box>
+              <Text fontWeight={"bold"}>Title</Text>
+              <Text>{post.title}</Text>
+            </Box>
+            <Box>
+              <Text fontWeight={"bold"}>Body</Text>
+              <Text>{post.body}</Text>
+            </Box>
+          </Box>
+        );
+      })}
+    </Box>
+  );
+};
+
+export default Posts;
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const posts = await res.json();
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  };
+}
