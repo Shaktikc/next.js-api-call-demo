a624a2c5091b1757c355ef581c676c52426dfe8a
Shakti Kc <shakti3462@gmail.com>
2022-06-22 11:45:59 +0545

    changing the port

--- pages/ISR/index.js
+++ pages/ISR/index.js
@@ -8,27 +8,9 @@ export default IncrementalStaticRegeneration;
 
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
-// the path has not been generated.
-export async function getStaticPaths() {
-  const res = await fetch("https://.../posts");
-  const posts = await res.json();
-
-  // Get the paths we want to pre-render based on posts
-  const paths = posts.map((post) => ({
-    params: { id: post.id },
-  }));
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: blocking } will server-render pages
-  // on-demand if the path doesn't exist.
-  return { paths, fallback: "blocking" };
-}
-
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
 // revalidation is enabled and a new request comes in
 export async function getStaticProps() {
-  const res = await fetch("https://.../posts");
+  const res = await fetch("http://localhost:3000/products/1");
   const posts = await res.json();
 
   return {
