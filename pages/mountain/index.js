e8842c3d128e32714cc5ebc9a5a49e8cc7376f49
Shakti Kc <shakti3462@gmail.com>
2022-08-16 16:37:22 +0545

    working on ui animation testing

--- pages/mountain/index.js
+++ pages/mountain/index.js
@@ -8,35 +8,69 @@ import NextImage from "next/image";
 
 const ProductImage = chakra(NextImage, {
   baseStyle: { maxH: 120, maxW: 120 },
-  shouldForwardProp: (prop) => ["width", "height", "src", "alt"].includes(prop),
+  shouldForwardProp: (prop) =>
+    [
+      "width",
+      "height",
+      "src",
+      "alt",
+      "quality",
+      "placeholder",
+      "blurDataURL",
+      "loader ",
+    ].includes(prop),
 });
 const Mountain = () => {
   return (
     <Box pos="relative">
-      <Box width={"100vw"} height="100vh" pos="relative" zIndex={"1"}>
-        <Image src={mingmadai} alt="nicee" objectFit="cover" layout="fill" />
-      </Box>
-      <Box zIndex="-1">
-        <Image src={Rectangle} alt="nicee" objectFit="cover" layout="fill" />
-      </Box>
+      <ProductImage
+        src={mingmadai}
+        alt="nicee"
+        objectFit="cover"
+        layout="fill"
+      />
 
       <ProductImage
-        src={cloud}
-        width={120}
-        height={120}
-        w="200"
-        h="200"
-        borderWidth={5}
-        borderStyle="solid"
-        pos="absolute"
-        zIndex={"2"}
-
-        // top="400px"
+        src={Rectangle}
+        alt="nicee"
+        objectFit="cover"
+        layout="fill"
       />
 
       {/* <Box>
         <Image src={cloud} alt="nicee" objectFit="cover" layout="fill" />
       </Box> */}
+      {/* <div style={{ position: "absolute", top: "0" }}>
+        <ProductImage
+          src={cloud}
+          width={120}
+          height={120}
+          w="200"
+          h="200"
+          borderWidth={5}
+          borderStyle="solid"
+          pos="absolute"
+          zIndex={"2"}
+          top="400px"
+          layout="fixed"
+          bg="green"
+        />
+      </div> */}
+      <Box pos="absolute" bg="black" top="0">
+        <ProductImage
+          src={cloud}
+          width={120}
+          height={120}
+          w="200"
+          h="200"
+          borderWidth={5}
+          borderStyle="solid"
+          pos="absolute"
+          zIndex={"2"}
+          top="400px"
+          layout="fixed"
+        />
+      </Box>
     </Box>
   );
 };
