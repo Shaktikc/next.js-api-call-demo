a7641757b2b11fc8fe1157275a6a1af96b30652f
Shakti Kc <shakti3462@gmail.com>
2022-08-16 14:58:54 +0545

    working on ui animation testing

--- pages/mountain/index.js
+++ pages/mountain/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Image from "next/image";
+import mingmadai from "../../imges/mingmadai.png";
+import { Box } from "@chakra-ui/react";
 
 const Mountain = () => {
-  return <div>Mountain</div>;
+  return (
+    <Box width={"100vw"} height="100vh" pos="relative">
+      <Image src={mingmadai} alt="nicee" objectFit="cover" layout="fill" />
+    </Box>
+  );
 };
 
 export default Mountain;
