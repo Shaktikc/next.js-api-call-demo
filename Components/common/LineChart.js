b39f17469b8f85700c664382b500c3f8527312f6
Shakti Kc <shakti3462@gmail.com>
2022-08-18 10:55:02 +0545

    working on mountian

--- Components/common/LineChart.js
+++ Components/common/LineChart.js
@@ -24,12 +24,20 @@ ChartJS.register(
 export const options = {
   responsive: true,
   plugins: {
-    legend: {
-      position: "top",
+    legend: false,
+    tooltips: {
+      callbacks: {
+        label: (tooltipItem) => `${tooltipItem.yLabel}: ${tooltipItem.xLabel}`,
+        title: () => null,
+      },
     },
-    title: {
-      display: true,
-      text: "Chart.js Line Chart",
+  },
+  scales: {
+    y: {
+      display: false, // Hide Y axis labels
+    },
+    x: {
+      display: false, // Hide X axis labels
     },
   },
 };
@@ -40,16 +48,9 @@ export const data = {
   labels,
   datasets: [
     {
-      label: "Dataset 1",
       data: [34, 45, 555, 56],
       borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: [34, 45, 555, 56],
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
   ],
 };
