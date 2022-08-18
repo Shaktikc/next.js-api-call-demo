ac02489430bc26fc3435b36ba4189bf171881dfe
Shakti Kc <shakti3462@gmail.com>
2022-08-18 15:12:21 +0545

    next mountain animatio

--- Components/common/LineChart.js
+++ Components/common/LineChart.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -47,18 +47,44 @@ const labels = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 export function LineChart() {
   const chartRef = useRef();
+  const [arrayData, setArrayData] = useState([
+    150, 500, 200, 350, 250, 320, 200, 280, 150,
+  ]);
+
   const onClick = (event) => {
     console.log(getElementAtEvent(chartRef.current, event));
+    if (getElementAtEvent(chartRef.current, event)[0]?.index === 1) {
+      // console.log("7");
+      setArrayData([150, 500, 200, 350, 250, 320, 200, 280, 150]);
+      return;
+    }
+    if (getElementAtEvent(chartRef.current, event)[0]?.index === 3) {
+      // console.log("33");
+      setArrayData([150, 350, 200, 500, 200, 320, 200, 280, 150]);
+      return;
+    }
+    if (getElementAtEvent(chartRef.current, event)[0]?.index === 5) {
+      console.log("5");
+      setArrayData([150, 300, 200, 300, 200, 500, 200, 280, 150]);
+      return;
+    }
+    if (getElementAtEvent(chartRef.current, event)[0]?.index === 7) {
+      console.log("7");
+      setArrayData([150, 300, 200, 300, 200, 280, 200, 500, 150]);
+      return;
+    }
   };
   {
     data1: [150, 500, 200, 350, 250, 320, 200, 280, 150];
-    data2: [150, 350, 200, 500, 250, 320, 200, 280, 150];
+    data2: [150, 350, 200, 500, 200, 320, 200, 280, 150];
+    data3: [150, 300, 200, 300, 200, 500, 200, 280, 150];
+    data4: [150, 300, 200, 300, 200, 280, 200, 500, 150];
   }
   const data = {
     labels,
     datasets: [
       {
-        data: [150, 500, 200, 350, 250, 320, 200, 280, 150],
+        data: arrayData,
         borderColor: "rgb(255, 99, 132)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
