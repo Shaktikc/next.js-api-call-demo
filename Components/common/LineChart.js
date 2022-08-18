202768843d86c4ada631eb50cb53d5ff4f1ccab0
Shakti Kc <shakti3462@gmail.com>
2022-08-18 14:39:55 +0545

    fixing the design issue

--- Components/common/LineChart.js
+++ Components/common/LineChart.js
@@ -33,33 +33,36 @@ export const options = {
       },
     },
   },
-  scales: {
-    y: {
-      display: false, // Hide Y axis labels
-    },
-    x: {
-      display: false, // Hide X axis labels
-    },
-  },
+  // scales: {
+  //   y: {
+  //     display: false, // Hide Y axis labels
+  //   },
+  //   x: {
+  //     display: false, // Hide X axis labels
+  //   },
+  // },
 };
 
 const labels = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      data: [150, 500, 200, 350, 250, 320, 200, 280, 150],
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-  ],
-};
 
 export function LineChart() {
   const chartRef = useRef();
   const onClick = (event) => {
     console.log(getElementAtEvent(chartRef.current, event));
+  };
+  {
+    data1: [150, 500, 200, 350, 250, 320, 200, 280, 150];
+    data2: [150, 350, 200, 500, 250, 320, 200, 280, 150];
+  }
+  const data = {
+    labels,
+    datasets: [
+      {
+        data: [150, 500, 200, 350, 250, 320, 200, 280, 150],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
   };
   return (
     <Line ref={chartRef} options={options} data={data} onClick={onClick} />
